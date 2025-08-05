b2d6649efb26fbfc1f22a104cf564a4bbc85ed99
akbarwijayaa <jimsaber02@gmail.com>
2025-08-05 15:26:11 +0700

    refactor: extract constants for market deployment and reward rates in Deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -7,6 +7,21 @@ import {Reward} from "../src/Reward.sol";
 import {MockUSDT} from "../src/mocks/MockUSDT.sol";
 
 contract Deploy is Script {
+
+    string MARKET_NAME = "FR-A";
+    uint256 constant MAX_SUPPLY = 1_000_000;
+
+    uint256 constant ONE_MONTH = 30 days;
+    uint256 constant THREE_MONTH = 90 days;
+    uint256 constant SIX_MONTH = 180 days;
+    uint256 constant ONE_YEAR = 365 days;
+
+    uint256 constant ONE_MONTH_APY = 400;
+    uint256 constant THREE_MONTH_APY = 600;
+    uint256 constant SIX_MONTH_APY = 800;
+    uint256 constant ONE_YEAR_APY = 1000;
+
+
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
@@ -14,11 +29,18 @@ contract Deploy is Script {
         MockUSDT usdt = new MockUSDT();
         console.log("MockUSDT deployed at:", address(usdt));
 
-        Market market = new Market("FR-A", address(usdt), 1_000_000);
+        Market market = new Market(MARKET_NAME, address(usdt), MAX_SUPPLY);
         console.log("Market deployed at:", address(market));
 
         Reward reward = new Reward(address(usdt), address(market), address(this));
         console.log("Reward contract address:", address(reward));
+
+        reward.setRewardRate(ONE_MONTH, ONE_MONTH_APY);
+        reward.setRewardRate(THREE_MONTH, THREE_MONTH_APY);
+        reward.setRewardRate(SIX_MONTH, SIX_MONTH_APY);
+        reward.setRewardRate(ONE_YEAR, ONE_YEAR_APY);
+
+        console.log("Reward rates set!");
 
         vm.stopBroadcast();
     }
